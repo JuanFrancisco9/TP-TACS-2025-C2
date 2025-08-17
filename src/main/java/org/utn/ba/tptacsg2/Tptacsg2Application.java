088413524e97448b028f4e3991b7d36dd100549a
a-sandoval <asandoval@frba.utn.edu.ar>
2025-08-17 20:38:00 -0300

    spring boot app andando

---
+++ src/main/java/org/utn/ba/tptacsg2/Tptacsg2Application.java
@@ -0,0 +1,13 @@
+package org.utn.ba.tptacsg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptacsg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptacsg2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/utn/ba/tptacsg2/Tptacsg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utn.ba.tptacsg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptacsg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
