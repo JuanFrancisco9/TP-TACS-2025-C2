7d5c0372fc3500a46175e6361b14fdbd64d4811e
Valentina Alberio <valentina.alberio@despegar.com>
2025-08-30 21:02:26 -0300

    Endpoint para cancelar la inscripcion al evento

--- src/main/java/org/utn/ba/tptacsg2/dtos/UsuarioDto.java
+++ src/main/java/org/utn/ba/tptacsg2/dtos/UsuarioDto.java
@@ -1,11 +1,9 @@
 package org.utn.ba.tptacsg2.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class UsuarioDto {
     private Long id;
     @NotBlank(message = "Debe completar todos los campos")
