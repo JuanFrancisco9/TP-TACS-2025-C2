95ae0b9783eed4f29b17ee84d3d7c48f229fa25e
TomasPS <tomas.sager@gmail.com>
2025-09-15 21:12:50 -0300

    Init categorias

---
+++ backend/src/main/java/org/utn/ba/tptacsg2/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package org.utn.ba.tptacsg2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.utn.ba.tptacsg2.models.events.Categoria;
+import org.utn.ba.tptacsg2.repositories.CategoriaRepository;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    @Autowired
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public List<Categoria> getCategorias() {
+        return this.categoriaRepository.getCategorias();
+    }
+}
