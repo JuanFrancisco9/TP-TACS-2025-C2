610353ddd35eec52d5d070955cfb58c9bc72c710
TomasPS <tomas.sager@gmail.com>
2025-10-01 11:25:56 -0300

    EstadoEventoRepository

--- backend/src/main/java/org/utn/ba/tptacsg2/repositories/EstadoEventoRepository.java
+++ backend/src/main/java/org/utn/ba/tptacsg2/repositories/EstadoEventoRepository.java
@@ -1,11 +1,13 @@
 package org.utn.ba.tptacsg2.repositories;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.utn.ba.tptacsg2.models.events.EstadoEvento;
 import org.utn.ba.tptacsg2.models.events.TipoEstadoEvento;
 import org.utn.ba.tptacsg2.models.inscriptions.EstadoInscripcion;
 import org.utn.ba.tptacsg2.models.inscriptions.TipoEstadoInscripcion;
+import org.utn.ba.tptacsg2.repositories.db.EstadoEventoRepositoryDB;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,22 +15,18 @@ import java.util.List;
 
 @Repository
 public class EstadoEventoRepository {
-    private final List<EstadoEvento> estadosDeEventos = new ArrayList<>();
+    private EstadoEventoRepositoryDB repository;
+
+    @Autowired
+    public EstadoEventoRepository (EstadoEventoRepositoryDB repository) {
+        this.repository = repository;
+    }
 
     public void guardarEstadoEvento(EstadoEvento estadoEvento) {
-        this.estadosDeEventos.add(estadoEvento);
+        repository.save(estadoEvento);
     }
 
     public EstadoEvento getEstadoInscripcionById(String id) {
-        return this.estadosDeEventos.stream()
-                .filter(estado -> estado.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
-
-    @PostConstruct
-    public void init() {
-        guardarEstadoEvento(new EstadoEvento("1", TipoEstadoEvento.CONFIRMADO,  LocalDateTime.now(), null));
-        guardarEstadoEvento(new EstadoEvento("2", TipoEstadoEvento.CONFIRMADO,  LocalDateTime.now(), null));
+        return repository.findById(id).orElse(null);
     }
 }
